832cf8c49c6424da2bde1e15cace1a3311337395
Aleh Koralko <oleg.korolko.work@gmail.com>
2018-05-25 15:14:13 +0300

    Fix extra json properties in Entity classes

--- src/main/java/com/karalko/model/BaseEntity.java
+++ src/main/java/com/karalko/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.karalko.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,6 +15,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @ToString
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 abstract class BaseEntity implements Serializable {
 
     @Id
